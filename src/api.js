314450cf6e39ae69c26c7abbfcb2204fa66841d3
Robert Gajelomo <robertgajelomo@Rhoy-Mac.local>
2023-03-28 13:46:46 +0800

    counter fix

--- src/api.js
+++ src/api.js
@@ -6,55 +6,79 @@ const csv = require('csv-parser');
 const { countReset } = require('console');
 var axios = require('axios');
 
-var pre_compile_data = [];
-let counter = { fails: 0, success: 0 };
 
-var interval = 500;
-
+var interval = 1000;
+let credentials = { "username": "", "password": "" };
+//let counter = { fails: 0, success: 0 };
 (async () => {
     const client = await local_connection.connect();
     await client.query('LISTEN cmw_listener');
     client.on('notification', function (data) {
+        getConfig(parseInt(data.payload));
         //console.log("data", JSON.parse(data.payload));
-        setTimeout(() => {
-            local_connection.query(`SELECT * FROM cmw_config where triggerstatus='active' and sending ='true' and status !='sending'`).then(res => {
-                const data = res.rows;
-                console.log('Config queue count : ', res.rowCount);
+        function getConfig(dataload) {
+            setTimeout(() => {
+                local_connection.query(`SELECT * FROM cmw_config where triggerstatus='active' and sending ='true' and status !='sending'`).then(res => {
+                    const data = res.rows;
+                    console.log('Config queue count : ', res.rowCount);
 
-                data.forEach(row => {
-                    fs.createReadStream('./uploads/data_leads/' + row.data_leads)
-                        .pipe(csv())
-                        .on('data', function (data) {
-                            try {
-                                pre_compile_data.push(JSON.stringify({ 'player_token': data.playertoken, 'country': data.country, 'message': data.message, 'platform': data.platform }));
-                                //console_log(data.playertoken + ',' + data.country + ',' + data.message + ',' + data.platform);
-                                //constructData(data.playertoken, data.country, data.message, data.platform);
-                            } catch (err) {
-                                console_log(err);
-                                console_log('error contact number');
-                            }
+                    console.log('payload :', dataload);
+                    const callback = dataload == res.rowCount;
+                    if (callback) {
+                        data.forEach(row => {
+
+                            var pre_compile_data = [];
+                            var dynamic_contact = row.config_id;
+                            pre_compile_data[dynamic_contact];
+                            //console.log(dynamic_contact);
+                            fs.createReadStream('./uploads/data_leads/' + row.data_leads)
+                                .pipe(csv())
+                                .on('data', function (data) {
+                                    try {
+                                        pre_compile_data.push(JSON.stringify({ 'player_token': data.playertoken, 'country': data.country, 'message': data.message, 'platform': data.platform }));
+                                        //console_log(data.playertoken + ',' + data.country + ',' + data.message + ',' + data.platform);
+                                        //constructData(data.playertoken, data.country, data.message, data.platform);
+                                    } catch (err) {
+                                        console_log(err);
+                                        console_log('error contact number');
+                                    }
+                                })
+                                .on('end', () => {
+                                    //console_log('done');
+                                    //console.log(pre_compile_data);
+
+                                    constructData(row.config_id, pre_compile_data, row.campaign_name);
+                                    local_connection.query(`update cmw_config set status= 'sending' where config_id=${row.config_id}`);
+                                });
                         })
-                        .on('end', () => {
-                            //console_log('done');
-                            //console.log(pre_compile_data);
-                            constructData(pre_compile_data);
-                        });
+                    }
+
                 })
-            })
-        }, "5000")
+            }, "10000")
+
+        }
+
     });
+    //counter.success = 0;
+    //counter.fails = 0;
+
+
 })();
-/*
 
 
-    */
+function constructData(config_id, pre_compile_data, campaign_name) {
 
-function constructData(pre_compile_data) {
 
-    let total_instant = 0;
+    let dynamic_counter = {};
+    let counter_name = 'counter';
+
+    dynamic_counter[counter_name] = { fails: 0, success: 0 };
+    //console.log(dynamic_counter.counter);
+
+
+
     let query_instant = 0;
     pre_compile_data.forEach(function (el, index) {
-        total_instant++
         setTimeout(async function () {
 
             var obj = JSON.parse(el);
@@ -70,50 +94,80 @@ function constructData(pre_compile_data) {
                 } else {
                     data.forEach(async row => {
                         if (obj.platform == 'sms') {
-                            await sendSMS(obj.message, row.phone_number)
+                            await sendSMS(obj.message, row.phone_number, obj.country)
                                 .then(function (response) {
                                     //console.log('success');
-                                    console.log(`Status : ${obj.player_token} Sent`);
-                                    counter.success++;
+                                    console_log(`Status : ${obj.player_token} Sent :` + `Campaign:${campaign_name}`);
+                                    //counter.success++;
                                     query_instant++
+                                    dynamic_counter.counter.success++
                                 })
                                 .catch(function (error) {
                                     //console.log('error');
-                                    console.log(`Status : ${obj.player_token} Failed`);
-                                    counter.fails++;
+                                    console_log(`Status : ${obj.player_token} Failed :` + `Campaign:${campaign_name}`);
+                                    //counter.fails++;
+                                    dynamic_counter.counter.fails++
                                     query_instant++
                                 })
-                                .finally(function () {
+                                .finally(async function () {
                                     if (pre_compile_data.length == query_instant) {
-                                        console.log(`Campaign: test, Result: ${counter.success} sent, ${counter.fails} failed`);
+
+                                        console_log(`Campaign: ${campaign_name}, Result: ${dynamic_counter.counter.success} sent, ${dynamic_counter.counter.fails} failed`);
+                                        dynamic_counter.counter.success = 0;
+                                        dynamic_counter.counter.fails = 0;
+                                        pre_compile_data.length = 0;
+                                        local_connection.query(`update cmw_config set triggerstatus= 'inactive' , status = 'sent' where config_id=${config_id}`);
+
+                                        /*
+                                        console_log(`Campaign: ${campaign_name}, Result: ${counter.success} sent, ${counter.fails} failed`);
                                         counter.success = 0;
                                         counter.fails = 0;
+                                        pre_compile_data.length = 0;
+                                        local_connection.query(`update cmw_config set triggerstatus= 'inactive' , status = 'sent' where config_id=${config_id}`);
+                                        */
                                     }
                                 });
                         }
                         else if (obj.platform == 'email') {
                             //sendEmail(player_token, country, message, platform, row.email);
                         }
-
                     });
                 }
             });
         }, index * interval);
-
-
     });
+}
 
+function apiAccount(country_code) {
+    if (country_code == 'TH') {
+        credentials.username = "N8Og84n1";
+        credentials.password = "qyu5C1DA";
+    } else if (country_code == 'VN') {
+        credentials.username = "9J3CtNdM";
+        credentials.password = "m4K1c25P";
+    } else {
+        credentials.username = "";
+        credentials.password = "";
+    }
+}
 
+function storeConfigHistory() {
 
 }
 
-async function sendSMS(player_token, country, message, platform, phone_number) {
+
+async function sendSMS(message, phone_number, country_code) {
+
+    apiAccount(country_code);
+
+
+    const encodedParamValue = encodeURIComponent(message);
     return new Promise(async (resolve, reject) => {
 
         var config = {
             method: 'get',
             maxBodyLength: Infinity,
-            url: `https://my.sms-smart.com/rest/send_sms?from=+639611573154&to=INV&message=${message}&username=9J3CtNdM&password=m4K1c25P`
+            url: `https://my.sms-smart.com/rest/send_sms?from=+639611573154&to=09611573154&message=${encodedParamValue}&username=${credentials.username}&password=${credentials.password}`
         };
 
         await axios(config)
@@ -121,9 +175,11 @@ async function sendSMS(player_token, country, message, platform, phone_number) {
                 resolve();
             })
             .catch(function (error) {
+                //console.error(error);
                 reject();
             });
     });
+
 }
 
 async function sendEmail(player_token, country, message, platform, email) {
