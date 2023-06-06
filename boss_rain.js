b3e7ac03ca9cf4989f13c86e4d89a080a507b420
Robert Gajelomo <robertgajelomo@Rhoy-Mac.local>
2023-06-06 14:29:21 +0800

    succesfully working rain api

--- boss_rain.js
+++ boss_rain.js
@@ -1,21 +1,23 @@
-const axios = require('axios'); 
+const axios = require('axios');
+const FormData = require('form-data');
+const https = require('https'); 
 
-console.log('teststs');
-var config = {
-    method: 'post',
+let config = {
+    method: 'get',
     maxBodyLength: Infinity,
-    url: 'https://172.31.1.12:8069/emailsender/api/1?email=robert.gajelomo123@everlounge.net',
-    headers: { 
-        'Authorization': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2NzkzODc4NDYsImp0aSI6InVMMTNaVG1ETndjRVh1TlF0dm43Y3c9PSIsImlzcyI6IiIsIm5iZiI6MTY3OTM4Nzg0NiwiZXhwIjoxNjc5Mzg4MjA2LCJkYXRhIjp7InVzZXJuYW1lIjoicmFpbiIsInBhc3N3b3JkIjoicG9naTY5Iiwic2l0ZV9rZXkiOiJxcXFxcTY5In19.vmVRS4_aaBGvx_kCQO_lga7LWgAFUgGWmLyWeIrLBBc', 
-        'Cookie': 'ci_session=qiie2l6mtsu39pe6mk1ie7tn83srb4om; ci_session=3qebtnrctcekndkrbmhl72peno56bs56; ci_session=t8uk6rdjibkkkf3v6enambu8qh58i330'
-      }
+    httpsAgent: new https.Agent({ rejectUnauthorized: false }), // Add this line
+    url: 'https://13.229.158.52:8069/emailsender/api/1?email=efrain.jorque@gmail.com',
+    headers: {
+        'Authorization': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2NzkzODc4NDYsImp0aSI6InVMMTNaVG1ETndjRVh1TlF0dm43Y3c9PSIsImlzcyI6IiIsIm5iZiI6MTY3OTM4Nzg0NiwiZXhwIjoxNjc5Mzg4MjA2LCJkYXRhIjp7InVzZXJuYW1lIjoicmFpbiIsInBhc3N3b3JkIjoicG9naTY5Iiwic2l0ZV9rZXkiOiJxcXFxcTY5In19.vmVRS4_aaBGvx_kCQO_lga7LWgAFUgGWmLyWeIrLBBc',
+        'Cookie': 'ci_session=qiie2l6mtsu39pe6mk1ie7tn83srb4om; ci_session=3qebtnrctcekndkrbmhl72peno56bs56; ci_session=mu2rfjorbt95nbgqpu0jncf9mu807nla'
+    }
 };
 
-
-axios(config)
-    .then(function (response) {
-     console.log(response);
+axios.request(config)
+    .then((response) => {
+        console.log(JSON.stringify(response.data));
     })
-    .catch(function (error) {
+    .catch((error) => {
         console.log(error);
     });
+
