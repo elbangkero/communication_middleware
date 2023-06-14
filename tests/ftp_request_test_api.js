25ff2f87018be270cf008f974b62a07cd59aa1b7
Robert Gajelomo <robertgajelomo@Rhoy-Mac.local>
2023-06-14 21:11:28 +0800

    remove userAGent

--- tests/ftp_request_test_api.js
+++ tests/ftp_request_test_api.js
@@ -6,7 +6,6 @@ const https = require('https');
 let config = {
     method: 'post',
     maxBodyLength: Infinity,
-    httpsAgent: new https.Agent({ rejectUnauthorized: false }), // Add this line
     url: 'https://13.229.158.52:8069/Emailsender/api/',
     headers: {
         'Authorization': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2NzkzODc4NDYsImp0aSI6InVMMTNaVG1ETndjRVh1TlF0dm43Y3c9PSIsImlzcyI6IiIsIm5iZiI6MTY3OTM4Nzg0NiwiZXhwIjoxNjc5Mzg4MjA2LCJkYXRhIjp7InVzZXJuYW1lIjoicmFpbiIsInBhc3N3b3JkIjoicG9naTY5Iiwic2l0ZV9rZXkiOiJxcXFxcTY5In19.vmVRS4_aaBGvx_kCQO_lga7LWgAFUgGWmLyWeIrLBBc',
