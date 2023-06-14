41d07e5405c5494fd60405f918e13b7bd7720795
Robert Gajelomo <robertgajelomo@Rhoy-Mac.local>
2023-06-14 21:17:53 +0800

    change url

--- tests/ftp_request_test_api.js
+++ tests/ftp_request_test_api.js
@@ -6,7 +6,8 @@ const https = require('https');
 let config = {
     method: 'post',
     maxBodyLength: Infinity,
-    url: 'https://13.229.158.52:8069/Emailsender/api/',
+    httpsAgent: new https.Agent({ rejectUnauthorized: false }), // Add this line
+    url: 'https://172.31.1.12:8069/Emailsender/api/',
     headers: {
         'Authorization': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2NzkzODc4NDYsImp0aSI6InVMMTNaVG1ETndjRVh1TlF0dm43Y3c9PSIsImlzcyI6IiIsIm5iZiI6MTY3OTM4Nzg0NiwiZXhwIjoxNjc5Mzg4MjA2LCJkYXRhIjp7InVzZXJuYW1lIjoicmFpbiIsInBhc3N3b3JkIjoicG9naTY5Iiwic2l0ZV9rZXkiOiJxcXFxcTY5In19.vmVRS4_aaBGvx_kCQO_lga7LWgAFUgGWmLyWeIrLBBc',
         'Content-Type': 'application/x-www-form-urlencoded',
@@ -17,7 +18,7 @@ let config = {
 
 axios.request(config)
     .then((response) => {
-        console.log(response);
+        console.log(JSON.stringify(response.data));
     })
     .catch((error) => {
 
