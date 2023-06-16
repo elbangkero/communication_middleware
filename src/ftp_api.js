aae9d85c76dcdd471b853bb00731582908215adb
Robert Gajelomo <robertgajelomo@Rhoy-Mac.local>
2023-06-16 16:56:15 +0800

    chage interval

--- src/ftp_api.js
+++ src/ftp_api.js
@@ -5,7 +5,7 @@ const https = require('https');
 var interval = 3000;
 
 const MAX_VERIFICATION_ATTEMPTS = 4;
-let VERIFICATION_INTERVAL = 5000;
+let VERIFICATION_INTERVAL = 60000;
 
 let verificationAttempts = 1;
 
