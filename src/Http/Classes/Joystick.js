9a61a04b3ca096c41bb18f1cf66faafa641cc46f
Robert Gajelomo <robertgajelomo@ip-192-168-254-108.ap-southeast-1.compute.internal>
2024-03-06 23:33:01 +0800

    add delay from api player info

--- src/Http/Classes/Joystick.js
+++ src/Http/Classes/Joystick.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-
+const interval = `${process.env.INTERVAL_TIME}`;
 /*
 async function SetUserInfoFromJoystick(player_token) {
     return new Promise(async (resolve, reject) => {
@@ -16,39 +16,42 @@ async function SetUserInfoFromJoystick(player_token) {
 async function SetUserInfoFromJoystick(token) {
 
     return new Promise(async (resolve, reject) => {
-        let config = {
-            method: 'get',
-            maxBodyLength: Infinity,
-            url: `https://afun-playerinfo-ap-prod.vitruviandata.com/player?token=${token}`,
-            headers: {
-                'Authorization': 'Basic bmltYnVzOndeN3FCUCpOJlRaZQ=='
-            },
-            timeout: 60000
-        };
 
-        axios.request(config)
-            .then((response) => {
-                if (response.data.results.length === 0) {
-                    reject('Invalid {PlayerToken}');
-                } else {
-                    resolve({
-                        'email': response.data.results[0].profile.email,
-                        'phone_number': response.data.results[0].profile.phoneNumber,
-                        'classificationcode': response.data.results[0].tags,
-                        'brandcode': response.data.results[0].info.siteCode,
-                        'country': response.data.results[0].profile.country,
-                        'playername': response.data.results[0].profile.firstName + ' ' + response.data.results[0].profile.lastName
-                    });
-                }
+        setTimeout(async () => {
+            let config = {
+                method: 'get',
+                maxBodyLength: Infinity,
+                url: `https://afun-playerinfo-ap-prod.vitruviandata.com/player?token=${token}`,
+                headers: {
+                    'Authorization': 'Basic bmltYnVzOndeN3FCUCpOJlRaZQ=='
+                },
+                timeout: 60000
+            };
 
-            })
-            .catch((error) => {
-                if (error.code === 'ECONNABORTED') {
-                    reject('Request Timeout From Vitruvian');
-                } else {
-                    reject(error);
-                }
-            });
+            axios.request(config)
+                .then((response) => {
+                    if (response.data.results.length === 0) {
+                        reject('Invalid {PlayerToken}');
+                    } else {
+                        resolve({
+                            'email': response.data.results[0].profile.email,
+                            'phone_number': response.data.results[0].profile.phoneNumber,
+                            'classificationcode': response.data.results[0].tags,
+                            'brandcode': response.data.results[0].info.siteCode,
+                            'country': response.data.results[0].profile.country,
+                            'playername': response.data.results[0].profile.firstName + ' ' + response.data.results[0].profile.lastName
+                        });
+                    }
+
+                })
+                .catch((error) => {
+                    if (error.code === 'ECONNABORTED') {
+                        reject('Request Timeout From Vitruvian');
+                    } else {
+                        reject(error);
+                    }
+                });
+        }, interval);
     });
 
 }
