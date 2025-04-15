0192b4869259adfb3c099112496ebef3bd93cd18
meliaana <melia.ana938@gmail.com>
2025-04-16 02:51:55 +0400

    feat: replace hardcoded API key with dynamic lookup

--- src/views/report_proxyAPI.js
+++ src/views/report_proxyAPI.js
@@ -1,21 +1,29 @@
 const axios = require("axios");
+const { ElasticEmailAccount } = require("../provider_api/elastic_email");
 
 exports.getElasticReport = async (_req, _res) => {
   const config_id = _req.params.config_id;
+  const provider_code = _req.query.provider_code;
+  const country = _req.query.country_code;
+
+  if (!provider_code || !country || !config_id) {
+    return _res.status(400).json({ error: "Missing required parameter" });
+  }
+
   const url = `https://api.elasticemail.com/v4/statistics/channels/${config_id}`;
 
   try {
+    const apikeyObj = await ElasticEmailAccount(provider_code, country);
     const response = await axios.get(url, {
       headers: {
-        "X-ElasticEmail-ApiKey":
-          "2B4AC4328B8B3B4DC426FDDF39010B5164AD7573E3C3006B0F11E03BB7A4BAFDA7F0FAFCE5AC86AD35F41D419CEBC4A7",
+        "X-ElasticEmail-ApiKey": apikeyObj.apikey,
         "Content-Type": "application/json",
       },
     });
 
-    return response.data;
+    _res.status(200).json(response.data);
   } catch (error) {
     console.error("Failed to fetch Elastic Email report:", error.message);
-    throw new Error("Elastic Email API request failed");
+    _res.status(500).json({ error: "Elastic Email API request failed" });
   }
 };
