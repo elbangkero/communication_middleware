466b5d837bfd579a7dd40f3d0672c2e0d6765002
elbangkero <gajelomo16@gmail.com>
2025-05-05 23:22:34 +0800

    Refresh Report everyweek

--- src/custom_site_api/bulk_sending_report.js
+++ src/custom_site_api/bulk_sending_report.js
@@ -31,13 +31,40 @@ function GetBulkCampaignList() {
     });
 }
 
-async function InsertCampaignTrigger(config_id, sent_at, status) {
+function GetBulkCampaignListNextRun() {
+    return new Promise((resolve, reject) => {
+        local_connection.query(` 
+        select cmrt.config_id,TO_CHAR(cmrt.next_run::date, 'YYYY-MM-DD') as created_at,cc.campaign_name,count_run from cmw_campaign_report_trigger cmrt
+            left join cmw_config cc on cc.config_id = cmrt.config_id
+            where cmrt.next_run = NOW()::date
+            and cmrt.status = 'success'
+            and cmrt.count_run < 5;
+ 
+            `, (err, res) => {
+            if (err) {
+                reject(`GetBulkCampaignListNextRun[Error]: ${err.message}`);
+            } else {
+                resolve(res.rows || res);
+            }
+        });
+    });
+}
+
+
+async function InsertCampaignTrigger(config_id, sent_at, status, next_run, count_run) {
+    const nextRunValue = status === 'success' ? `'${next_run}'` : null;
     return new Promise(async (resolve, reject) => {
-        local_connection.query(`
-INSERT INTO cmw_campaign_report_trigger
-(config_id, sent_at, created_at, status)
-VALUES(${config_id}, '${sent_at}', CURRENT_TIMESTAMP, '${status}');`, (err, res) => {
+        local_connection.query(`INSERT INTO cmw_campaign_report_trigger (config_id, sent_at, created_at, status, next_run, count_run) VALUES (${config_id}, '${sent_at}', CURRENT_TIMESTAMP, '${status}',${nextRunValue},${count_run});`, (err, res) => {
             err ? reject(`InsertCampaignTrigger[Error]: ${err.message}`) : resolve(res);
+        });
+    });
+}
+
+
+async function UPDATEcampaignTrigger(config_id, status, next_date_run, count_run) {
+    return new Promise(async (resolve, reject) => {
+        local_connection.query(`update cmw_campaign_report_trigger set count_run = '${count_run}' ,next_run='${next_date_run}',status='${status}' where config_id = '${config_id}'`, (err, res) => {
+            err ? reject(`UPDATEcampaignTrigger[Error]: ${err.message}`) : resolve(res);
         });
     });
 }
@@ -69,13 +96,27 @@ DO UPDATE SET
     });
 }
 
+
+function PlusSevenDays(formattedDate) {
+    const date = new Date(formattedDate);
+
+    date.setDate(date.getDate() + 7);
+
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+
+    const newDate = `${year}-${month}-${day}`;
+    return newDate;
+
+}
+
 function GetReport() {
     GetBulkCampaignList()
         .then(result => {
-            console.log(`Found ${result.length} campaigns to process`);
-
             result.forEach(async item => {
                 const formattedDate = item.created_at;
+
                 let config = {
                     method: 'get',
                     maxBodyLength: Infinity,
@@ -85,7 +126,10 @@ function GetReport() {
                 await axios.request(config)
                     .then(async (response) => {
                         const status = response.status === 200 ? 'success' : 'failed';
-                        await InsertCampaignTrigger(item.config_id, formattedDate, status);
+                        const convertedDate = new Date(formattedDate);
+                        const next_date_run = PlusSevenDays(convertedDate);
+
+                        await InsertCampaignTrigger(item.config_id, formattedDate, status, next_date_run, 1);
                         if (response.status === 200) {
                             await InsertCampaignHistory(item.config_id, item.campaign_name, response.data.Recipients, response.data.EmailTotal,
                                 response.data.SmsTotal, response.data.Delivered, response.data.Bounced, response.data.InProgress,
@@ -94,7 +138,7 @@ function GetReport() {
                         }
                     })
                     .catch(async (error) => {
-                        await InsertCampaignTrigger(item.config_id, formattedDate, 'failed');
+                        await InsertCampaignTrigger(item.config_id, formattedDate, 'failed', null, 0);
                     });
             });
         })
@@ -103,6 +147,47 @@ function GetReport() {
         });
 }
 
+
+function NextRunReport() {
+    GetBulkCampaignListNextRun()
+        .then(result => {
+            result.forEach(async item => {
+                const formattedDate = item.created_at;
+
+                let config = {
+                    method: 'get',
+                    maxBodyLength: Infinity,
+                    url: `${base_url}/elastic_report/${item.config_id}`,
+                };
+
+
+                await axios.request(config)
+                    .then(async (response) => {
+                        const status = response.status === 200 ? 'success' : 'failed';
+                        const convertedDate = new Date(formattedDate);
+                        const next_date_run = PlusSevenDays(convertedDate);
+
+                        await UPDATEcampaignTrigger(item.config_id, status, next_date_run, item.count_run + 1);
+                        if (response.status === 200) {
+                            await InsertCampaignHistory(item.config_id, item.campaign_name, response.data.Recipients, response.data.EmailTotal,
+                                response.data.SmsTotal, response.data.Delivered, response.data.Bounced, response.data.InProgress,
+                                response.data.Opened, response.data.Clicked, response.data.Unsubscribed,
+                                response.data.Complaints, response.data.Inbound, response.data.ManualCancel, response.data.NotDelivered);
+                        }
+                    })
+                    .catch(async (error) => {
+                        await UPDATEcampaignTrigger(item.config_id, 'failed', next_date_run, item.count_run);
+                    });
+            });
+        })
+        .catch(error => {
+            console.error(`Error in GetBulkCampaignListNextRun: ${error}`);
+        });
+}
+
 cron.schedule('0 0 * * *', () => {
     GetReport();
+    NextRunReport();
 });
+GetReport();
+NextRunReport();
